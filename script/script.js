c69d3f616e786a52a47d72fd80dbad974613dce6
zutanov <big_smoke9@mail.ru.com>
2021-11-22 01:36:04 +0600

    js-number

---
+++ script/script.js
@@ -0,0 +1,55 @@
+//1:
+let num = 7
+console.log(num)
+
+//2:
+let a = 10
+let b = 2
+let c = 5
+console.log(a + b + c)
+
+//3:
+let d = 100
+console.log( d * 2)
+
+//4:
+let e = 67
+console.log(e % 7)
+
+//5:
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+//6:
+let f = 15
+let g = 2
+let result = f + g
+console.log(result)
+
+//7:
+let h = 30
+let i = 15
+let j = h + i
+console.log(j % 4)
+
+//8:
+let k = 17
+let l = 10
+let m = k - l
+let n = 22
+let result2 = m + n
+console.log(result2)
+
+//9:
+let o = 10
+let p = 99
+let result3 = o + p
+console.log(result3)
+
+//10:
+let name = "лет"
+let number = 10
+let result4 = number + " " + name
+console.log(result4)
